44834eee52e71078dafe5575a83a8e2a758c5925
bruic87 <bruic@outlook.com>
2023-07-27 08:54:28 -0300

    'exemplo de uso de comparadores de igualdade'

---
+++ igualdade_igualdadeEstrita.js
@@ -0,0 +1,25 @@
+// Diferença entre igualdade e igualdade estrita
+
+// Exemplo de comparação com igualdade
+let resposta_usuario = "";
+
+if(resposta_usuario == 0)
+{
+    console.log("resposta correta");
+}
+else
+{
+    console.log("resposta incorreta");
+}
+
+// Exemplo de comparação com igualdade estrita
+let resposta_usuario = "";
+
+if(resposta_usuario === 0)
+{
+    console.log("resposta correta");
+}
+else
+{
+    console.log("resposta incorreta");
+}
